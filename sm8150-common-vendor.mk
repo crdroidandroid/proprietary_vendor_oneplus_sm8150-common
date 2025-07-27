e288aa8952d8ef358922642515d2ceda8325bb6f
LuK1337 <priv.luk@gmail.com>
2025-07-27 21:39:04 +0200

    sm8150-common: Update WFD vendor blobs from sweet_k6a OS1.0.19.0.THGMIX
    Change-Id: I6f8c4da7384e85ab4a65b7d084bda18381a16b05

--- sm8150-common-vendor.mk
+++ sm8150-common-vendor.mk
@@ -842,7 +842,6 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3-service-qti.xml \
     manifest_oplus_cameramdm_2.0.xml \
     vendor.qti.gnss@4.0-service.xml \
-    manifest_android.hardware.drm@1.3-service.wfdhdcp.xml \
     manifest_android.hardware.drm@1.3-service.widevine.xml \
     manifest_oplusSensor.xml \
     manifest_oplus_fingerprint.xml \
@@ -901,7 +900,6 @@ PRODUCT_PACKAGES += \
     thermal-engine \
     time_daemon \
     vppservice \
-    wfdhdcphalservice \
     wfdvndservice \
     wifidisplayhalservice \
     xtra-daemon \
